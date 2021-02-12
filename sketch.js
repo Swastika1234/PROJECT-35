52c5b8700fc5e877d11c4b3f7b8f3f5d0e28b36d
Swastika1234 <77337384+Swastika1234@users.noreply.github.com>
2021-02-12 12:45:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball;
+var database,position;
+var balloon,bg;
+
+function preload(){
+    bg=loadImage("background.png")
+    balloon=loadImage("airBalloon.png")
+}
+
+function setup(){
+database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.addImage(balloon)
+    ball.scale=0.5
+    var locOfChild=database.ref("ball/position")
+    locOfChild.on("value",readOp)
+}
+
+function draw(){
+    background(bg);
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+}
+function readOp(data){
+    position=data.val();
+    ball.x=position.x
+    ball.y=position.y
+}
